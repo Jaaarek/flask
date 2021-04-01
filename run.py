854dd3c74e8825f0db5170345ea8c3b562f57ece
burzamiszcz <burzynskihubert@gmail.com>
2021-04-01 16:06:52 +0200

    tworzymy logowanie

--- run.py
+++ run.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id = 1, username = 'admin', password='admin'))
+print(users)
+
 @app.route('/')
 def main():
-    return 'Hello world'
-
-@app.route('/innastrona')
-def innastrona():
-    return 'Siemanko'
-
-@app.route('/client/<numer>')
-def client(numer):
-    return f'Klient o podanym numerze {numer} to...'
-
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
